53f7cf7bb7ef0412716d4d6b308cecf97cb7e3e9
Arviena <arviena1004@gmail.com>
2022-07-14 20:58:30 +0700

    Initial commit

---
+++ src/main/java/com/coffeeshop/loginregis/LoginregisApplication.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.loginregis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginregisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginregisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffeeshop/loginregis/LoginregisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffeeshop.loginregis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginregisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
